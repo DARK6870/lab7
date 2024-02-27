8d91e00fa287636dcf3120645af0ee4ecc1493b4
DARK6870 <tsymbalvlad.6870@gmail.com>
2024-02-27 20:01:20 +0200

    init

---
+++ lab7/lab7/MainWindow.xaml.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls.Primitives;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace lab7
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public DataTable table { get; set; }
+        public Massiv mas { get; set; }
+        
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            table = new DataTable();
+            mas = new Massiv();
+            mas.data = new int[15];
+            
+            FillData();
+            DataColumn column = table.Columns.Add("Number", typeof(int));
+            datagrid.RowBackground = Brushes.AliceBlue;
+            SetData();
+        }
+
+        private void ShowMsg(string text)
+        {
+            result_label.Content = text;
+        }
+
+        private void FillData()
+        {
+            Random rnd = new Random();
+            for(int i = 0; i < 15; i++)
+            {
+                mas.data[i] = rnd.Next(1, 15);
+            }
+        }
+
+        private void SetData()
+        {
+            table.Rows.Clear();
+            foreach (var num in mas.data)
+            {
+                table.Rows.Add(num);
+            }
+
+            datagrid.ItemsSource = table.DefaultView;
+        }
+
+        private int[] GetDataFromTable()
+        {
+            var numbers = table.AsEnumerable().Select(row => row.Field<int>("number")).ToArray();
+            return numbers;
+        }
+
+
+        private void Pr_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                mas.data = GetDataFromTable();
+                int res = mas.Pr();
+                ShowMsg($"Произведение чисел равно: {res}");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void max_btn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                mas.data = GetDataFromTable();
+                int max = mas.Max();
+
+                ShowMsg($"Максимальный элемент массива: {max}");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void odd_btn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                mas.data = GetDataFromTable();
+                int nechet = mas.Odd();
+                int chet = mas.Even();
+                ShowMsg($"Количество чётных: {chet}\nКоличесвто нечётнх: {nechet}");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void delete_btn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                mas.data = GetDataFromTable();
+                int number = Convert.ToInt32(numb_tb.Text);
+                mas.DeleteElement(number);
+                SetData();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
---
+++ lab7/lab7/Massiv.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab7
+{
+    public class Massiv
+    {
+        public int[] data { get; set; }
+        public int Pr()
+        {
+            return data.Aggregate((x, y) => x * y);
+        }
+
+        public int Max()
+        {
+            return data.Max();
+        }
+
+        public int Even()
+        {
+            return data.Where(p => p % 2 == 0).Count();
+        }
+
+        public int Odd()
+        {
+            return data.Where(p => p % 2 == 1).Count();
+        }
+
+        public void DeleteElement(int x)
+        {
+            int[] newarr = data.Where(value => value != x).ToArray();
+            data = newarr;
+        }
+
+
+        public void OrderAsk()
+        {
+            data.Order();
+        }
+        public void OrderDesk()
+        {
+            data.OrderDescending();
+        }
+    }
+}
